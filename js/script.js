b96294e560267777947d857673f49c37ad99d1ff
Sylvester Norrbjörk <sn222jn@student.lnu.se>
2014-05-14 11:37:40 +0200

    flash update

---
+++ js/script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".content ol ul").hide();
+
+    $(".content ol li").click(function (e) {
+        $(this).children("ul").toggle(
+        { left: 200 }, {
+            duration: 'slow',
+            easing: 'linear'
+        })
+
+        e.stopPropagation();
+    });
+});
+
